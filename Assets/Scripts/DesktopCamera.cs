aff2bba9222a4f4aecddbaabd05c9974d0e10fa7
dearsip <dearsip@users.noreply.github.com>
2021-02-08 17:22:50 +0900

    デスクトップ上立体視の追加（スペースキーで切り替え）

---
+++ Assets/Scripts/DesktopCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesktopCamera : MonoBehaviour
+{
+    public Camera left, right;
+    private int mode = 0;
+    private Rect rLeft, rRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        left.enabled = false;
+        right.enabled = false;
+        rLeft = left.rect;
+        rRight = right.rect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            switch (mode++ % 3)
+            {
+                case 0:
+                    left.enabled = true;
+                    right.enabled = true;
+                    rLeft.x = 0.5f;
+                    rRight.x = 0f;
+                    left.rect = rLeft;
+                    right.rect = rRight;
+                    break;
+                case 1:
+                    rLeft.x = 0f;
+                    rRight.x = 0.5f;
+                    left.rect = rLeft;
+                    right.rect = rRight;
+                    break;
+                case 2:
+                    left.enabled = false;
+                    right.enabled = false;
+                    break;
+            }
+    }
+}
