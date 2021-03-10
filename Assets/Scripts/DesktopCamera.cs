8c03e3e837ff80d57bd5ca9bc4f8da4654d3e491
dearsip <45082750+dearsip@users.noreply.github.com>
2021-03-10 16:40:51 +0900

    固定カメラ追加

--- Assets/Scripts/DesktopCamera.cs
+++ Assets/Scripts/DesktopCamera.cs
@@ -4,41 +4,58 @@ using UnityEngine;
 
 public class DesktopCamera : MonoBehaviour
 {
-    public Camera left, right;
+    public Camera left, right, fix, fLeft, fRight;
     private int mode = 0;
-    private Rect rLeft, rRight;
+    private Rect rLeft, rRight, frLeft, frRight;
     // Start is called before the first frame update
     void Start()
     {
         left.enabled = false;
         right.enabled = false;
+        fix.enabled = false;
+        fLeft.enabled = false;
+        fRight.enabled = false;
         rLeft = left.rect;
         rRight = right.rect;
+        frLeft = fLeft.rect;
+        frRight = fRight.rect;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
-            switch (mode++ % 3)
+            switch (mode++ % 6)
             {
                 case 0:
                     left.enabled = true;
                     right.enabled = true;
-                    rLeft.x = 0.5f;
-                    rRight.x = 0f;
-                    left.rect = rLeft;
-                    right.rect = rRight;
+                    left.rect = rRight;
+                    right.rect = rLeft;
                     break;
                 case 1:
-                    rLeft.x = 0f;
-                    rRight.x = 0.5f;
                     left.rect = rLeft;
                     right.rect = rRight;
                     break;
                 case 2:
                     left.enabled = false;
                     right.enabled = false;
+                    fix.enabled = true;
+                    break;
+                case 3:
+                    fix.enabled = false;
+                    fLeft.enabled = true;
+                    fRight.enabled = true;
+                    fLeft.rect = frRight;
+                    fRight.rect = frLeft;
+                    break;
+                case 4:
+                    fLeft.rect = frLeft;
+                    fRight.rect = frRight;
+                    break;
+                case 5:
+                    fLeft.enabled = false;
+                    fRight.enabled = false;
                     break;
             }
     }
