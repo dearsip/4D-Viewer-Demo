3fb5c39b3e579f0afc8c3e4bd898e08bccf0e3ed
dearsip <45082750+dearsip@users.noreply.github.com>
2022-08-04 18:00:11 +0900

    デスクトップからガイド表示（クリック）

---
+++ Assets/Scripts/DesktopLaser.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace VRUiKits.Utils
+{
+    public class DesktopLaser : MonoBehaviour
+    {
+        public Camera c;
+        LineRenderer lr;
+
+        #region MonoBehaviour Callbacks
+        void Start()
+        {
+            lr = GetComponent<LineRenderer>();
+        }
+
+        void LateUpdate()
+        {
+            lr.SetPosition(0, transform.position);
+            RaycastHit hit;
+            if (Input.GetMouseButton(0) && Physics.Raycast(c.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                lr.enabled = true;
+                if (hit.collider)
+                {
+                    lr.SetPosition(1, hit.point);
+                }
+            }
+            else
+            {
+                lr.enabled = false;
+            }
+        }
+        #endregion
+
+    }
+}
