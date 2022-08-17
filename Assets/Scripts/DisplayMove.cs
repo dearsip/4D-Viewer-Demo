3152c4df64c092cd8e6d8a7d6f0b23882a676091
dearsip <pdaeisr@gmail.com>
2022-08-17 12:41:04 +0900

    画面サイズに応じ画面移動量増加

--- Assets/Scripts/DisplayMove.cs
+++ Assets/Scripts/DisplayMove.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using UnityEngine.UI;
 
 // Display の位置を動かす。
 public class DisplayMove : MonoBehaviour
@@ -9,11 +10,12 @@ public class DisplayMove : MonoBehaviour
     public SteamVR_Input_Sources hand;
     public SteamVR_Action_Boolean grab;
     public SteamVR_Action_Pose pose;
+    public Slider size;
     private Quaternion relarot;
     void Update()
     {
         if (grab.GetState(hand)) {
-            transform.position += pose.GetLocalPosition(hand) - pose.GetLastLocalPosition(hand);
+            transform.position += (pose.GetLocalPosition(hand) - pose.GetLastLocalPosition(hand))*size.value;
             relarot = pose.GetLocalRotation(hand) * Quaternion.Inverse(pose.GetLastLocalRotation(hand));
             relarot.x = 0; relarot.z = 0;
             transform.rotation *= relarot;
